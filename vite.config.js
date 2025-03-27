12334d27ea0b46b23e0d8d2228802732f329b07e
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-03-27 07:11:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    base:"/Apple-Clone/",
+    base:"./Apple-Clone/",
     org: "jsm-x9",
     project: "javascript-react"
   })],
